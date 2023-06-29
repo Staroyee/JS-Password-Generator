1cd414965e60d1c84f0da7d3f983bfbec4a67d1f
Staroyee <danmasefield@gmail.com>
2023-06-29 16:47:12 +0930

    Added JS code to create a loop if user doesnt select the required criteria

--- script.js
+++ script.js
@@ -30,6 +30,19 @@ function generatePassword() {
   /*d.*/var number = confirm("Click OK to include numeric characters.");
   /*e.*/var specialChar = confirm("Click OK to include special characters.");
 
+// 5. Create 'if' and 'if else' statements to check if the minimum required criteria has been selected.
+/*If the minimum criteria was not met, an alert will remind the user of the requirement and start the promps from the beginning.*/
+if (passwordLength < 8 || passwordLength > 128) {
+  alert("Please try again: Choose a minimum number of 8 or maximum number of 128")
+  var passwordLength = prompt("Choose a password length with a minimum of 8 characters and a maximum of 128 characters");
+} else if (lowercaseChar === false && uppercaseChar === false && number === false && specialChar === false) {
+  alert("Please choose a minimum of one password criteria.")
+  var lowercaseChar = confirm("Click OK to include lowercase characters.");
+  var uppercaseChar = confirm("Click OK to include uppercase characters.");
+  var number = confirm("Click OK to include numeric characters.");
+  var specialChar = confirm("Click OK to include special characters.");
+}
+
 
 }
 
